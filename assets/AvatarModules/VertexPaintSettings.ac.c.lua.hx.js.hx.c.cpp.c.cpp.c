ac43d7c4416b4d4ef7d429c206e8f420548bbae8
Cgun-art | michael :) <migueloproio903@gmail.com>
2026-05-01 10:52:19 -0300

    Create VertexPaintSettings.ac.c.lua.hx.js.hx.c.cpp.c.cpp.c

---
+++ assets/AvatarModules/VertexPaintSettings.ac.c.lua.hx.js.hx.c.cpp.c.cpp.c
@@ -0,0 +1,9 @@
+#include <GLFW/glsl3.ac>
+
+local Avatar
+
+    if Avatar => keep:
+
+return:
+
+warpTex:(baseTex, addtex(textures/smile_0.png)/ warp=plane pos;MakeVertices ai.vertexgen(model, Gemini.3.1-Pro) / makeFace add, vertice, addTex(textures/smile_0.png), UVCHANGE=FALSE, MATCHTEXVERTICECHANGE /* ai *\ ),
